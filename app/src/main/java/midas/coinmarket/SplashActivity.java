765d68af76f69be60062558af8f6e3fa32299f88
dinhdv <dinhdv@hanelsoft.vn>
2018-07-14 15:09:46 +0700

    dinhdv commit coin

--- app/src/main/java/midas/coinmarket/SplashActivity.java
+++ app/src/main/java/midas/coinmarket/SplashActivity.java
@@ -3,8 +3,17 @@ package midas.coinmarket;
 
 import android.accounts.Account;
 import android.accounts.AccountManager;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Handler;
+import android.util.Base64;
+import android.util.Log;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 import midas.coinmarket.controller.LoginActivity;
 import midas.coinmarket.controller.MainActivity;
@@ -21,6 +30,7 @@ public class SplashActivity extends BaseActivity {
 
     @Override
     public void initFunction() {
+        printHashKey(SplashActivity.this);
         mAccount = AccountUtils.getAccountUser(SplashActivity.this, AccountManager.get(this));
         new Handler().postDelayed(new Runnable() {
             @Override
@@ -29,5 +39,21 @@ public class SplashActivity extends BaseActivity {
                 finish();
             }
         }, 2000);
+    }
+
+    public void printHashKey(Context pContext) {
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                String hashKey = new String(Base64.encode(md.digest(), 0));
+                Log.i("KEY", "printHashKey() Hash Key: " + hashKey);
+            }
+        } catch (NoSuchAlgorithmException e) {
+            Log.e("KEY", "printHashKey()", e);
+        } catch (Exception e) {
+            Log.e("KEY", "printHashKey()", e);
+        }
     }
 }
--- app/src/main/java/midas/coinmarket/controller/LoginActivity.java
+++ app/src/main/java/midas/coinmarket/controller/LoginActivity.java
@@ -1,16 +1,29 @@
 package midas.coinmarket.controller;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.view.View;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+
+import org.json.JSONObject;
+
+import java.util.Arrays;
 
 import butterknife.OnClick;
 import midas.coinmarket.R;
@@ -22,6 +35,7 @@ import midas.coinmarket.utils.BaseActivity;
 public class LoginActivity extends BaseActivity implements GoogleApiClient.OnConnectionFailedListener {
     private GoogleApiClient mGoogleApiClient;
     private static final int RC_SIGN_IN = 123;
+    private CallbackManager callbackManager;
 
     @Override
     public int getLayoutId() {
@@ -38,16 +52,56 @@ public class LoginActivity extends BaseActivity implements GoogleApiClient.OnCon
                 .enableAutoManage(this, this)
                 .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
                 .build();
+        // Facebook.
+        callbackManager = CallbackManager.Factory.create();
+        LoginManager.getInstance().registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        getDataAccount();
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        // App code
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        Toast.makeText(LoginActivity.this, "Cannot reigster with facebook", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    private void getDataAccount() {
+        GraphRequest req = GraphRequest.newMeRequest(AccessToken.getCurrentAccessToken(), new GraphRequest
+                .GraphJSONObjectCallback() {
+            @Override
+            public void onCompleted(JSONObject object, GraphResponse response) {
+                if (null != object && object.length() > 0) {
+                    String email = object.optString("email");
+                    UserObject userObject = new UserObject();
+                    userObject.setId(object.optString("id"));
+                    userObject.setEmail(email);
+                    startActivity(new Intent(LoginActivity.this, ConfirmLoginActivity.class).putExtra(AppConstants.INTENT.USER, userObject));
+                } else {
+                    Toast.makeText(LoginActivity.this, "Cannot reigster with facebook", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        Bundle parameters = new Bundle();
+        parameters.putString("fields", "email");
+        req.setParameters(parameters);
+        req.executeAsync();
     }
 
     @OnClick({R.id.btn_facebook, R.id.btn_google})
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.btn_facebook:
-                startActivity(new Intent(LoginActivity.this, MainActivity.class));
-                finish();
+                LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList
+                        ("public_profile", "email"));
                 break;
-
             case R.id.btn_google:
                 signIn();
                 break;
@@ -61,6 +115,7 @@ public class LoginActivity extends BaseActivity implements GoogleApiClient.OnCon
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == RC_SIGN_IN) {
             GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
